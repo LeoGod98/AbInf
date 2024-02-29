824ce3f41be82877048195758cbd1dce115b1277
LeoGod98 <75836900+LeoGod98@users.noreply.github.com>
2024-02-29 10:48:08 +0100

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
+cd
 mkdir consegna_godeas
 cd consegna_godeas
 wget -O "godeas.py" "https://raw.githubusercontent.com/LeoGod98/AbInf/main/godeas.py"
 wget -O "get_file.bash" "https://raw.githubusercontent.com/LeoGod98/AbInf/main/get_file.bash"
-cp get_file.bash godeas.py consegna_godeas
-chmod a+rwx "consegna_godeas/get_file.bash"
-chmod a+rwx "consegna_godeas/godeas.py"
+chmod a+rwx "get_file.bash"
+chmod a+rwx "godeas.py"
 ./get_file.bash
